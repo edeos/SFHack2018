49b7c3ef5180f1d584085966488655217bd21ba8
edeos <pasha1507@ukr.net>
2018-11-11 11:57:43 +0200

    added store & update methods in WalletController

--- app/Http/Controllers/Api/WalletController.php
+++ app/Http/Controllers/Api/WalletController.php
@@ -4,8 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Wallet;
 
 class WalletController extends Controller
 {
-    //
+
+	public function store(Request $request)
+	{
+
+		$student = $request->student_id . ':example.com';
+
+		$wallet = new Wallet;
+		$wallet->name = $student;
+		$wallet->wallet = 'wallet';
+		$wallet->save();
+
+		return response()->json([
+			'wallet' => 'wallet',
+			'public_key' => 'public key',
+			'private_key' => 'private key',
+		], 200);
+
+	}
+
+	public function update(Request $request)
+	{
+
+		$wallet = new Wallet;
+		$wallet->name = $request->student_id;
+		$wallet->wallet = $request->wallet;
+		$wallet->save();
+
+		return response()->json([
+			'status' => 'success',
+		], 200);
+
+	}
+
 }
