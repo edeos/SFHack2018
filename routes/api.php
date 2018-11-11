02328287ce6e0da72ca320342202270cdc544a8d
edeos <pasha1507@ukr.net>
2018-11-11 11:58:58 +0200

    added routes for wallet methods

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('wallet/store', 'Api\WalletController@store');
+Route::post('wallet/update', 'Api\WalletController@update');
